7bd3dcdc14a5b671774d260fcffec635d9ad2c3b
seieric <30688855+seieric@users.noreply.github.com>
2019-12-28 20:31:07 +0900

    Enable GoDaddy

--- lib/go_daddy.rb
+++ lib/go_daddy.rb
@@ -1,5 +1,9 @@
 require __dir__ + '/env/api_keys'
 class GoDaddy
+  @headers = {
+    "Authorization" => "sso-key #{API_KEY}:#{API_SECRET}",
+    "Accept" => "applicaton/json"
+  }
   def self.is_available?(domain)
     domain = SimpleIDN.to_ascii(domain)
     param = URI.encode_www_form(domain: domain)
@@ -7,25 +11,18 @@ class GoDaddy
     req = Net::HTTP::Get.new(uri.request_uri)
     req.initialize_http_header(@headers)
     begin
-      response = Net::HTTP.new(uri.host, uri.port) do |http|
-        http.use_ssl = uri.scheme === "https"
-        http.open_timeout = 5
-        http.read_timeout = 10
-        http.request(req)
-      end
+      http = Net::HTTP.new(uri.host, uri.port)
+      http.use_ssl = uri.scheme === "https"
+      response = http.request(req)
 
-      if response.code = 200
+      if response.code == "200"
         data = JSON.parse(response.body)
-        data['available']
+        data['available'] #return1
+      else
+        nil #return2
       end
     rescue IOError => e
-      logger.error(e.full_message)
+      logger.error(e.full_message) #return3
     end
-  end
-  def initialize
-    @headers = {
-      "Authorization" => "sso-key #{API_KEY}:#{API_SECRET}",
-      "Accept" => "applicaton/json"
-    }
   end
 end
