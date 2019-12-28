d38831d28f190f59f6ca8bab01e88b374ab1d75c
seieric <30688855+seieric@users.noreply.github.com>
2019-12-28 20:21:38 +0900

    Add initializer: standard_libraries.rb

--- lib/go_daddy.rb
+++ lib/go_daddy.rb
@@ -1,11 +1,11 @@
-require './env/api_keys'
+require __dir__ + '/env/api_keys'
 class GoDaddy
-  def is_available?(domain)
-    domain = SinmpleIDN.to_ascii(domain)
+  def self.is_available?(domain)
+    domain = SimpleIDN.to_ascii(domain)
     param = URI.encode_www_form(domain: domain)
     uri = URI.parse("https://api.godaddy.com/v1/domains/available?#{param}")
     req = Net::HTTP::Get.new(uri.request_uri)
-    req.initialize_http_header(headers)
+    req.initialize_http_header(@headers)
     begin
       response = Net::HTTP.new(uri.host, uri.port) do |http|
         http.use_ssl = uri.scheme === "https"
@@ -13,8 +13,13 @@ class GoDaddy
         http.read_timeout = 10
         http.request(req)
       end
+
+      if response.code = 200
+        data = JSON.parse(response.body)
+        data['available']
+      end
     rescue IOError => e
-      e.full_message
+      logger.error(e.full_message)
     end
   end
   def initialize
