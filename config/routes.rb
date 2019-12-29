829c083943288db2d9a845137a221ec6b6357ff1
seieric <30688855+seieric@users.noreply.github.com>
2019-12-29 10:39:24 +0900

    Finish routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root "application#hello"
   get "prices", to: "domain_prices#index"
-  get "static_pages/about"
-  get "static_pages/contact"
-  get "static_pages/help"
-  get "static_pages/terms"
-  get "static_pages/policy"
+  get "about", to: "static_pages#about"
+  get "contact", to: "static_pages#contact"
+  get "help", to: "static_pages#help"
+  get "terms", to: "static_pages#terms"
+  get "policy", to: "static_pages#policy"
 end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -5,31 +5,31 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     @base_title = "最安ドメイン検索"
   end
   test "should get help" do
-    get static_pages_help_path
+    get help_path
     assert_response :success
     assert_select "title", "ヘルプ | #{@base_title}"
   end
 
   test "should get about" do
-    get static_pages_about_path
+    get about_path
     assert_response :success
     assert_select "title", "このサイトについて | #{@base_title}"
   end
 
   test "should get terms" do
-    get static_pages_terms_path
+    get terms_path
     assert_response :success
     assert_select "title", "利用規約 | #{@base_title}"
   end
 
   test "should get policy" do
-    get static_pages_policy_path
+    get policy_path
     assert_response :success
     assert_select "title", "プライバシーポリシー | #{@base_title}"
   end
 
   test "should get contact" do
-    get static_pages_contact_path
+    get contact_path
     assert_response :success
     assert_select "title", "お問い合わせ | #{@base_title}"
   end
