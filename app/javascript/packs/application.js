6cc1fa27c90d2fcd617c1045d12caf2d0bec3a77
seieric <30688855+seieric@users.noreply.github.com>
2020-01-04 12:49:56 +0900

    Finish design

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,6 +3,7 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 import 'bootstrap'
+import '@fortawesome/fontawesome-free/js/all' // Use fontawesome
 import '../src/application.scss'
 
 require("@rails/ujs").start()
