7d96a675e8e09e5eff0fc011c57e1bd9d77c335d
seieric <30688855+seieric@users.noreply.github.com>
2020-01-04 21:14:26 +0900

    Set smtp

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,6 +61,18 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "domainhikaku_production"
 
   config.action_mailer.perform_caching = false
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_url_options = { host: 'quiet-sierra-12183.herokuapp.com' }
+  ActionMailer::Base.delivery_method = :smtp
+  ActionMailer::Base.smtp_settings = {
+    user_name: ENV['SENDGRID_USERNAME'],
+    password: ENV['SENDGRID_PASSWORD'],
+    domain: "heroku.com",
+    address: "smtp.sendgrid.net",
+    port: 587,
+    authentication: :plain,
+    enable_starttls_auto: true
+  }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
