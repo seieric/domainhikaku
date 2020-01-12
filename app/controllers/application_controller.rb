f7300b1bb8133a1e442600ed5895c1cc25c976b4
seieric <30688855+seieric@users.noreply.github.com>
2020-01-12 22:53:48 +0900

    Redirect herokuapp.com

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception #Ajaxセキュリテートークンの生成
+
+  before_filter :ensure_domain
+  def ensure_domain
+    return unless /\.herokuapp.com/ =~ request.host
+    redirect_to "https://saiyasu.me#{request.path}", status: :moved_permanently
+  end
 end
