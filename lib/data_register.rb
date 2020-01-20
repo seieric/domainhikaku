e95cf5c74e206d5dfbef1c98fea39e6731b2b4b9
seieric <30688855+seieric@users.noreply.github.com>
2020-01-20 17:58:53 +0900

    Add Class crawler

--- lib/data_register.rb
+++ lib/data_register.rb
@@ -5,7 +5,7 @@ class DataRegister #各ユーザー定義タスクで使うデーターベース
       next unless price.is_a?(Array)  #配列じゃなければスキップ
       begin
           domain_price = DomainPrice.find_or_initialize_by(registrar: registrar, domain: domain)
-          domain_price.update_attributes!(update_price: price[-1], register_price: price[0])
+          domain_price.update_attributes!(renewal_price: price[-1], registration_price: price[0])
       rescue ActiveRecord::RecordInvalid => e
         logger.error(e.record.errors)
         p e.record.errors
@@ -14,7 +14,16 @@ class DataRegister #各ユーザー定義タスクで使うデーターベース
     end
   end
 
-  def self.add(domain, language = "en", )
+  def self.add(domain, lang = "en", registration, renewal, rgstr_no)
+    registrar = config(rgstr_no)
+    begin
+      price_set = DomainPrice.find_or_initialize_by(registrar: registrar, domain: domain, language: lang)
+      price_set.update_attributes!(registration_price: registration, renewal_price: renewal)
+    rescue ActiveRecord::RecordInvalid => e
+      logger.error(e.record.errors)
+      p e.record.errors
+    end
+  end
 
   def self.config(int)
     registrars = [
