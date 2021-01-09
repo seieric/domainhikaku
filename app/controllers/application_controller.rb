26359a82761f124ac6dcc339f0508d3f2aecc3b6
seieric <30688855+seieric@users.noreply.github.com>
2021-01-09 22:49:49 +0900

    disable redirect to saiyasu.me

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,3 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception #Ajaxセキュリテートークンの生成
-
-  before_action :ensure_domain
-  def ensure_domain
-    return unless /\.herokuapp.com/ =~ request.host
-    redirect_to "https://saiyasu.me#{request.path}", status: :moved_permanently
-  end
 end
